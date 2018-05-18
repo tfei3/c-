052846cbf99f92626d6e273d02cd23a810c3a21b
tfei3 <tfei3@ncsu.edu>
2018-05-18 12:55:02 -0400

    exercise/

---
+++ exercise/displayNA.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+  using namespace std;
+  string name;
+  string address;
+  cout<<"Please enter your name:";
+  cin>>name;
+  cout<<"Please enter your address:";
+  cin>>address;
+  cout<<"Your name is "<<name<<" And your address is "<<address;
+  return 0;
+}
